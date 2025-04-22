59ff3c5c1b94b84b7c677f6ab29d32855681d2cb
AqtoPy <rekikreki66@gmail.com>
2025-04-22 16:23:02 +0300

    Update vpk_reader.gd

--- vpk_reader.gd
+++ vpk_reader.gd
@@ -9,42 +9,42 @@ var files: Dictionary = {}
 func load(path: String) -> Error:
     files.clear()
     
-    var file := FileAccess.open(path, FileAccess.READ)
+    var file: FileAccess = FileAccess.open(path, FileAccess.READ)
     if file == null:
         return ERR_FILE_CANT_OPEN
     
     # Read header
-    var signature := file.get_32()
+    var signature: int = file.get_32()
     if signature != VPK_SIGNATURE:
         return ERR_FILE_UNRECOGNIZED
     
-    var version := file.get_32()
-    var tree_size := file.get_32()
+    var version: int = file.get_32()
+    var tree_size: int = file.get_32()
     
     # Skip header fields based on version
     if version == 1:
-        file.get_32() # Skip embedded data length
+        var _embedded_data_length: int = file.get_32() # Skip embedded data length
     elif version == 2:
-        file.get_32() # Skip embedded data length
-        file.get_32() # Skip archive MD5 length
+        var _embedded_data_length: int = file.get_32() # Skip embedded data length
+        var _archive_md5_length: int = file.get_32() # Skip archive MD5 length
     
     # Read file tree
     while file.get_position() < tree_size:
-        var extension := file.get_string()
-        var path_str := file.get_string()
-        var filename := file.get_string()
+        var extension: String = file.get_string()
+        var path_str: String = file.get_string()
+        var filename: String = file.get_string()
         
-        var crc := file.get_32()
-        var preload_bytes := file.get_16()
-        var archive_index := file.get_16()
-        var entry_offset := file.get_32()
-        var entry_length := file.get_32()
-        var terminator := file.get_16()
+        var crc: int = file.get_32()
+        var preload_bytes: int = file.get_16()
+        var archive_index: int = file.get_16()
+        var entry_offset: int = file.get_32()
+        var entry_length: int = file.get_32()
+        var terminator: int = file.get_16()
         
         if terminator != 0xffff:
             break
         
-        var full_path := path_str.path_join(filename + "." + extension)
+        var full_path: String = path_str.path_join(filename + "." + extension)
         files[full_path] = {
             "archive_index": archive_index,
             "offset": entry_offset,
@@ -62,7 +62,7 @@ func read_file(path: String) -> PackedByteArray:
         return PackedByteArray()
     
     var entry: Dictionary = files[path]
-    var data: PackedByteArray = entry["preload_data"]
+    var data: PackedByteArray = entry.get("preload_data", PackedByteArray())
     
     # TODO: Add support for reading from archive chunks
     # This would require handling the multi-part VPK files
