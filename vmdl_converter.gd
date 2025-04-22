93eda83a6542e27b7030b60f909c394d2102c023
AqtoPy <rekikreki66@gmail.com>
2025-04-22 17:56:50 +0300

    Update vmdl_converter.gd

--- vmdl_converter.gd
+++ vmdl_converter.gd
@@ -2,11 +2,17 @@
 class_name VMDLConverter
 extends RefCounted
 
+# Пользовательский ресурс для хранения данных скелета
+class SkeletonData extends Resource:
+    var bone_names: PackedStringArray
+    var bone_rests: Array[Transform3D]
+    var bone_parents: PackedInt32Array
+
 func convert_vmdl(data: PackedByteArray) -> Array:
     var mesh := ArrayMesh.new()
-    var skeleton := Skeleton3D.new()
+    var skeleton_data = SkeletonData.new()
     
-    # Пример: Создание тестового меша
+    # Пример данных (замените реальным парсингом VMDL)
     var vertices = PackedVector3Array([
         Vector3(0, 0, 0),
         Vector3(1, 0, 0),
@@ -15,15 +21,19 @@ func convert_vmdl(data: PackedByteArray) -> Array:
     
     var indices = PackedInt32Array([0, 1, 2])
     
+    # Заполнение данных меша
     var arrays = []
     arrays.resize(Mesh.ARRAY_MAX)
     arrays[Mesh.ARRAY_VERTEX] = vertices
     arrays[Mesh.ARRAY_INDEX] = indices
-    
     mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, arrays)
     
-    # Пример: Добавление тестовой кости
-    skeleton.add_bone("root")
-    skeleton.set_bone_rest(0, Transform3D.IDENTITY)
+    # Заполнение данных скелета (пример)
+    skeleton_data.bone_names = PackedStringArray(["root", "bone1"])
+    skeleton_data.bone_rests = [
+        Transform3D.IDENTITY,
+        Transform3D(Basis(), Vector3(1, 0, 0))
+    ]
+    skeleton_data.bone_parents = PackedInt32Array([-1, 0])
     
-    return [mesh, skeleton]
+    return [mesh, skeleton_data]
