98fca2dfc20a77012dda1fb04c4cc7b7ade7f0b8
AqtoPy <rekikreki66@gmail.com>
2025-04-22 18:51:17 +0300

    Update vtex_importer.gd

--- vtex_importer.gd
+++ vtex_importer.gd
@@ -4,16 +4,38 @@ extends EditorImportPlugin
 func _get_importer_name() -> String:
     return "csgo.vtex"
 
+func _get_visible_name() -> String:
+    return "CS:GO Texture"
+
 func _get_recognized_extensions() -> PackedStringArray:
-    return PackedStringArray(["vtex_c"])
+    return ["vtex_c"]
+
+func _get_save_extension() -> String:
+    return "png"
+
+func _get_resource_type() -> String:
+    return "Texture2D"
+
+func _get_priority() -> float:
+    return 1.0
+
+func _get_import_order() -> int:
+    return 200
+
+func _get_preset_count() -> int:
+    return 1
+
+func _get_preset_name(preset_index: int) -> String:
+    return "Default"
+
+func _get_import_options(path: String, preset_index: int) -> Array[Dictionary]:
+    return [{
+        "name": "generate_mipmaps",
+        "default_value": true
+    }]
 
 func _import(source_file: String, save_path: String, options: Dictionary, 
            platform_variants: Array[String], gen_files: Array[String]) -> Error:
-    var file = FileAccess.open(source_file, FileAccess.READ)
-    var data = file.get_buffer(file.get_length())
-    
-    var converter = preload("vtex_converter.gd").new()
-    var image = converter.convert_to_image(data)
-    
-    var texture = ImageTexture.create_from_image(image)
-return ResourceSaver.save(texture, "%s.png" % save_path)
+    var image = Image.create(1024, 1024, false, Image.FORMAT_RGBA8)
+    image.fill(Color(1, 0, 1)) # Тестовая текстура
+    return image.save_png("%s.%s" % [save_path, _get_save_extension()))
