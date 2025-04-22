ce9ce7ec59598f3a06435e91d55a1c7b54028155
AqtoPy <rekikreki66@gmail.com>
2025-04-22 19:28:56 +0300

    Update vtex_importer.gd

--- vtex_importer.gd
+++ vtex_importer.gd
@@ -1,6 +1,36 @@
 @tool
 extends EditorImportPlugin
 
+class VTEXConverter:
+    static func convert(data: PackedByteArray) -> Image:
+        var image = Image.new()
+        
+        # Проверка сигнатуры VTEX
+        if data.size() < 16 or data.slice(0, 4).get_string_from_ascii() != "VTEX":
+            push_error("Invalid VTEX format")
+            return _create_fallback_image()
+        
+        # Чтение параметров изображения
+        var width = data.decode_u32(8)
+        var height = data.decode_u32(12)
+        var format = data.decode_u32(16)
+        
+        # Простая реализация для DXT1/DXT5
+        if format == 0x1A:  # DXT1
+            image.set_data(width, height, false, Image.FORMAT_DXT1, data.slice(32))
+        elif format == 0x1B:  # DXT5
+            image.set_data(width, height, false, Image.FORMAT_DXT5, data.slice(32))
+        else:
+            push_error("Unsupported texture format: 0x%X" % format)
+            return _create_fallback_image()
+        
+        return image
+    
+    static func _create_fallback_image() -> Image:
+        var image = Image.create(64, 64, false, Image.FORMAT_RGBA8)
+        image.fill(Color(1, 0, 1)) # Розовый цвет
+        return image
+
 func _get_importer_name() -> String:
     return "csgo.vtex"
 
@@ -16,26 +46,19 @@ func _get_save_extension() -> String:
 func _get_resource_type() -> String:
     return "Texture2D"
 
-func _get_priority() -> float:
-    return 1.0
-
-func _get_import_order() -> int:
-    return 200
-
-func _get_preset_count() -> int:
-    return 1
-
-func _get_preset_name(preset_index: int) -> String:
-    return "Default"
-
-func _get_import_options(path: String, preset_index: int) -> Array[Dictionary]:
-    return [{
-        "name": "generate_mipmaps",
-        "default_value": true
-    }]
-
 func _import(source_file: String, save_path: String, options: Dictionary, 
            platform_variants: Array[String], gen_files: Array[String]) -> Error:
-    var image = Image.create(1024, 1024, false, Image.FORMAT_RGBA8)
-    image.fill(Color(1, 0, 1)) # Тестовая текстура
-    return image.save_png("%s.%s" % [save_path, _get_save_extension()))
+    # Чтение файла
+    var file = FileAccess.open(source_file, FileAccess.READ)
+    if file == null:
+        return ERR_FILE_CANT_OPEN
+    
+    var data = file.get_buffer(file.get_length())
+    
+    # Конвертация в изображение
+    var image = VTEXConverter.convert(data)
+    if image == null:
+        return ERR_PARSE_ERROR
+    
+    # Сохранение PNG
+    return image.save_png("%s.%s" % [save_path, _get_save_extension()])
