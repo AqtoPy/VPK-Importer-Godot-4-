dfa8239cdf669fe95ad391dd04e61b00e6291c89
AqtoPy <rekikreki66@gmail.com>
2025-04-22 17:34:44 +0300

    Update vmdl_importer.gd

--- vmdl_importer.gd
+++ vmdl_importer.gd
@@ -7,19 +7,44 @@ func _get_importer_name() -> String:
 func _get_recognized_extensions() -> PackedStringArray:
     return PackedStringArray(["vmdl_c"])
 
+# Добавляем метод указания расширения
+func _get_save_extension() -> String:
+    return "mesh"
+
+# Добавляем тип ресурса
+func _get_resource_type() -> String:
+    return "ArrayMesh"
+
+func _get_priority() -> float:
+    return 1.0
+
 func _import(source_file: String, save_path: String, options: Dictionary, 
            platform_variants: Array[String], gen_files: Array[String]) -> Error:
     var file = FileAccess.open(source_file, FileAccess.READ)
+    if file == null:
+        return ERR_FILE_CANT_OPEN
+        
     var data = file.get_buffer(file.get_length())
-    
     var converter = preload("vmdl_converter.gd").new()
     var result = converter.convert_vmdl(data)
+    
+    if result.size() < 2:
+        return ERR_PARSE_ERROR
     
     var mesh: ArrayMesh = result[0]
     var skeleton: Skeleton3D = result[1]
     
-    # Сохранение ресурсов
-    ResourceSaver.save(mesh, "%s.mesh" % save_path)
-    ResourceSaver.save(skeleton, "%s_skeleton.res" % save_path)
+    # Сохранение меша
+    var mesh_save_path = "%s.%s" % [save_path, _get_save_extension()]
+    var err = ResourceSaver.save(mesh, mesh_save_path)
+    if err != OK:
+        return err
+    
+    # Сохранение скелета как отдельного ресурса
+    if skeleton:
+        var skeleton_save_path = "%s_skeleton.res" % save_path
+        err = ResourceSaver.save(skeleton, skeleton_save_path)
+        if err != OK:
+            push_warning("Failed to save skeleton: " + str(err))
     
     return OK
